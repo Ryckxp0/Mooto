caeb8abd0aae624cf331a5639391d9bab5d38f4f
Ryckxp0 <141043150+Ryckxp0@users.noreply.github.com>
2025-01-04 17:48:41 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
-// Funcionalidade do formulário de contato
-document.getElementById('form-contato').addEventListener('submit', function (event) {
-    event.preventDefault(); // Evita o envio padrão do formulário
+function showServiceDetail(serviceId) {
+    // Esconde todas as seções de serviço
+    const serviceDetails = document.querySelectorAll('.service-detail');
+    serviceDetails.forEach(detail => {
+        detail.style.display = 'none';
+    });
 
-    const nome = document.getElementById('nome').value;
-    const email = document.getElementById('email').value;
-    const mensagem = document.getElementById('mensagem').value;
-
-    // Aqui você pode adicionar uma função para enviar os dados para um servidor ou fazer um alert
-    alert(`Mensagem enviada!\nNome: ${nome}\nE-mail: ${email}\nMensagem: ${mensagem}`);
-
-    // Limpar os campos após envio
-    document.getElementById('form-contato').reset();
-});
+    // Exibe a seção específica
+    const serviceDetail = document.getElementById(serviceId);
+    if (serviceDetail) {
+        serviceDetail.style.display = 'block';
+    }
+}
